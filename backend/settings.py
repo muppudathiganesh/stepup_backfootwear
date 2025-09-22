c6604a2b6792e9d3a3d19d25e24692d05cb6881f
muppudathiganesh <muppuraj11@gmail.com>
2025-09-22 18:28:02 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -76,9 +76,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "http://localhost:3000",
+   
     "http://127.0.0.1:5173",
     "https://stepup-frontend-ashy.vercel.app/",
    
